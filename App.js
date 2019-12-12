b71802bacc9e45d64357aeca90067d6e0d3c997c
Jérémy Barbet <jeremgraph@gmail.com>
2019-12-12 22:25:50 +0000

    Add reproduction issue code

--- App.js
+++ App.js
@@ -1,114 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {useState} from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+import MapboxGL from '@react-native-mapbox-gl/maps';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoiamVyZW1kc2duIiwiYSI6ImNrNDNhMmJyZjAyeTUzbG82dXl2enVxeDUifQ.v5ohmRBCluzuMNsWZq056A',
+);
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const urls = [
+  {
+    id: 1,
+    url:
+      'https://france-geojson.gregoiredavid.fr/repo/regions/centre-val-de-loire/region-centre-val-de-loire.geojson',
+  },
+  {
+    id: 2,
+    url:
+      'https://france-geojson.gregoiredavid.fr/repo/regions/ile-de-france/region-ile-de-france.geojson',
+  },
+  {
+    id: 3,
+    url:
+      'https://france-geojson.gregoiredavid.fr/repo/regions/normandie/region-normandie.geojson',
+  },
+];
 
-const App: () => React$Node = () => {
+export default () => {
+  const [activeUrl, setActiveUrl] = useState(urls[0].url);
+
+  const handlePress = index => {
+    setActiveUrl(urls.find(u => u.id === index).url);
+  };
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <TouchableOpacity
+        onPress={() => handlePress(1)}
+        style={{position: 'absolute', bottom: 80, left: 20, zIndex: 100}}>
+        <Text>ShapeSource 1</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => handlePress(2)}
+        style={{position: 'absolute', bottom: 100, left: 20, zIndex: 100}}>
+        <Text>ShapeSource 2</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => handlePress(3)}
+        style={{position: 'absolute', bottom: 120, left: 20, zIndex: 100}}>
+        <Text>ShapeSource 3</Text>
+      </TouchableOpacity>
+
+      <MapboxGL.MapView style={{flex: 1}}>
+        <MapboxGL.Camera
+          zoomLevel={4}
+          centerCoordinate={[2.237837, 48.890717]}
+        />
+
+        {/* If you remove key={activeUrl} it won't work anymore */}
+        <MapboxGL.ShapeSource id="shape" key={activeUrl} url={activeUrl}>
+          <MapboxGL.FillLayer
+            id="fill"
+            type="fill"
+            style={{fillColor: '#191a1a', fillOpacity: 0.4}}
+          />
+        </MapboxGL.ShapeSource>
+      </MapboxGL.MapView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
